acb3f47a32fd842a76bc5251315dec6dcc0b2586
Rimki <yhs693@naver.com>
2018-06-15 15:22:18 +0900

    .

--- rainsky/app/src/main/java/com/example/k/rainsky/MainActivity.java
+++ rainsky/app/src/main/java/com/example/k/rainsky/MainActivity.java
@@ -1,19 +1,12 @@
 package com.example.k.rainsky;
 
-import android.content.AsyncQueryHandler;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,9 +19,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
